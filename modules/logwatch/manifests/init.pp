e90b85fed28e2f5a72bcf3d9964191abc107e139
Paul Gear <github@libertysys.com.au>
2010-11-05 16:04:30 +1000

    Cheap & nasty logwatch config

---
+++ modules/logwatch/manifests/init.pp
@@ -0,0 +1,26 @@
+#
+# puppet module to install logwatch customisations
+#
+
+class logwatch {
+
+	package { "logwatch":
+		ensure	=> installed,
+	}
+
+	define logwatch_dir() {
+		file { "/etc/logwatch/$name":
+			ensure	=> directory,
+			owner	=> root,
+			group	=> root,
+			recurse	=> true,
+			ignore	=> [ ".svn", "*.orig", "*.patch", ],
+			source	=> "puppet:///modules/logwatch/$name",
+			require	=> Package["logwatch"],
+		}
+	}
+
+	logwatch_dir{ [ "conf", "scripts" ]: }
+
+}
+
