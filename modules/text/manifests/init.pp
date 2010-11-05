d7bcbae67a9a033a1dfcb58805e00e92a27ef751
Paul Gear <github@libertysys.com.au>
2010-11-05 17:52:48 +1000

    text module required by sysstat

---
+++ modules/text/manifests/init.pp
@@ -0,0 +1,37 @@
+# first two functions come from
+# http://www.debian-administration.org/articles/528
+#
+# License unknown
+
+class text {
+
+    define append_if_no_such_line($file, $line, $refreshonly = 'false') {
+	exec { "/bin/echo '$line' >> '$file'":
+	    unless		=> "/bin/grep -Fxqe '$line' '$file'",
+	    path		=> "/bin",
+	    refreshonly	=> $refreshonly,
+	}
+    }
+
+    define delete_lines($file, $pattern) {
+	exec { "sed -i -r -e '/$pattern/d' $file":
+	    path	=> "/bin",
+	    onlyif	=> "/bin/grep -E '$pattern' '$file'",
+	}
+    }
+
+    define comment_lines($file, $pattern) {
+	exec { "sed -i -r -e 's/($pattern)/##Commented out by puppet## \1/' $file":
+	    path	=> "/bin",
+	    onlyif	=> "/bin/grep -E '$pattern' '$file'",
+	}
+    }
+
+    define replace_lines($file, $pattern, $replace) {
+	exec { "sed -i -r -e 's/$pattern/$replace/' $file":
+	    path	=> "/bin",
+	    onlyif	=> "/bin/grep -E '$pattern' '$file'",
+	}
+    }
+
+}
