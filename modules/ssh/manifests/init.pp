52f0d1db066718f662627d1ec0a74978a6fa06b4
Paul Gear <github@libertysys.com.au>
2010-11-11 16:59:43 +1000

    Add private key & store dir management

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -29,6 +29,15 @@ class ssh {
 		require		=> Package[$pkg],
 	}
 
+	# include this only once on your puppetmaster
+	define storedir( $storedir = "$ssh::storedir" ) {
+		file { $storedir:
+			ensure		=> directory,
+			owner		=> "puppet",
+			recurse		=> true,
+		}
+	}
+
 	# only do this on your puppet master
 	define create_key( $user = "root", $type = "rsa", $storedir = "$ssh::storedir", $bits = 2048
 			) {
@@ -37,6 +46,12 @@ class ssh {
 			creates		=> "$storedir/id_$type.$name.pub",
 			logoutput	=> on_failure,
 			unless		=> "test -f $storedir/id_$type.$name",
+		}
+		file { "$storedir/id_$type.$name":
+			ensure		=> file,
+			owner		=> puppet,
+			mode		=> 640,
+			require		=> File[ $storedir ],
 		}
 	}
 
@@ -47,6 +62,34 @@ class ssh {
 			key	=> generate("/usr/bin/awk", "{printf \"%s\", \$2}", "$storedir/id_$type.$name.pub"),
 			type	=> $type,
 			user	=> $user,
+		}
+	}
+
+	# installs a private key and its corresponding public key from the puppet master on the client
+	define private_key( $user = "root", $type = "rsa", $storedir = "$ssh::storedir" ) {
+		# ugly hack for home dir - how to do better?
+		$home = $user ? {
+			root	=> "",
+			default	=> "/home",
+		}
+		file { "$home/$user/.ssh":
+			ensure	=> directory,
+			owner	=> $user,
+			mode	=> 700,
+		}
+		file { "$home/$user/.ssh/id_$type.pub":
+			ensure	=> file,
+			content	=> generate("/bin/cat", "$storedir/id_$type.$name.pub"),
+			owner	=> $user,
+			mode	=> 644,
+			require	=> File[ "$home/$user/.ssh" ],
+		}
+		file { "$home/$user/.ssh/id_$type":
+			ensure	=> file,
+			content	=> generate("/bin/cat", "$storedir/id_$type.$name"),
+			owner	=> $user,
+			mode	=> 600,
+			require	=> File[ "$home/$user/.ssh" ],
 		}
 	}
 
