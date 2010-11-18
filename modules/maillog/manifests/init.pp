830ac4c5d8d208d0861b9f80dd3289e63f75b7a2
Paul Gear <github@libertysys.com.au>
2010-11-18 16:02:14 +1000

    Activate new statistics script

--- modules/maillog/manifests/init.pp
+++ modules/maillog/manifests/init.pp
@@ -6,22 +6,32 @@
 
 class maillog {
 
+	# delete old script
 	file { "/usr/local/bin/check-pop-users":
+		ensure	=> absent,
+	}
+
+	file { "/usr/local/bin/check-mail-users":
 		ensure	=> file,
 		owner	=> root,
 		group	=> root,
 		mode	=> 755,
-		source	=> "puppet:///modules/maillog/check-pop-users";
+		source	=> "puppet:///modules/maillog/check-mail-users";
 	}
 
 	$mailto = "root@localhost"
 
+	# delete old cron job
 	cron_job { "000-check-pop-users":
+		enable		=> "false",
+	}
+
+	cron_job { "000-check-mail-users":
 		interval	=> "daily",
 		script		=> "#!/bin/sh
 # This script is managed by puppet - do not edit here!
 if [ `date +%w` = 0 ]; then
-	/usr/local/bin/check-pop-users | mail -s 'Weekly pop users report' $mailto
+	/usr/local/bin/check-mail-users | mail -s 'Weekly mail users report' $mailto
 fi
 ",
 	}
