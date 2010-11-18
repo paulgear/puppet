e987bd59eb62fe0eceabe0d4af5f5a0eb45befe0
Paul Gear <github@libertysys.com.au>
2010-11-18 13:39:41 +1000

    Add support for LVM snapshot without mount

--- modules/backup/manifests/lvm.pp
+++ modules/backup/manifests/lvm.pp
@@ -12,6 +12,30 @@ class backup::lvm {
 		source	=> "puppet:///modules/backup/bin/lvm-snapshot-mount",
 	}
 
+	# symlink lvm-snapshot to lvm-snapshot-mount
+	file { "/usr/local/bin/lvm-snapshot":
+		ensure	=> "/usr/local/bin/lvm-snapshot-mount",
+	}
+
+	# define lvm snapshot withOUT mount
+	define lvm_snapshot( $original_lv, $snapshot_lv, $snapshot_size, $pre_remove = "", $pre_snap = "", $post_snap = "" ) {
+		file { "/usr/local/etc/lvm-snapshot.conf":
+			ensure	=> file,
+			owner	=> root,
+			group	=> root,
+			mode	=> 644,
+			content	=> "# Managed by puppet - do not edit here!
+ORIGINAL_LV='$original_lv'
+SNAPSHOT_LV='$snapshot_lv'
+SNAPSHOT_SIZE='$snapshot_size'
+PRE_REMOVE='$pre_remove'
+PRE_SNAP='$pre_snap'
+POST_SNAP='$post_snap'
+",
+		}
+	}
+
+	# define lvm snapshot WITH mount
 	define snapshot_conf( $original_dir, $original_lv, $snapshot_dir, $snapshot_lv, $snapshot_size, $pre_remove = "", $pre_snap = "", $post_snap = "" ) {
 		file { "/usr/local/etc/lvm-snapshot-mount.conf":
 			ensure	=> file,
