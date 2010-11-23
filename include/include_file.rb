c72758fcbb8fc21c911e22af4fd2f59f2080b77a
Paul Gear <github@libertysys.com.au>
2010-11-23 17:13:40 +1000

    Next try

--- include/include_file.rb
+++ include/include_file.rb
@@ -1,6 +1,7 @@
 
 def include_file( name )
 	# add the path separator if it's not already there
+	templatedir = scope.lookupvar("templatedir")
 	templatedir.concat("/") unless templatedir.endswith("/")
 
 	includeheader = "#####\n# Included file: \n# " + templatedir + name + "\n#####\n"
