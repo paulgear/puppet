0918ff573174b3cfb1fd6ea40cc0493cc0336e53
Paul Gear <github@libertysys.com.au>
2010-11-24 08:12:09 +1000

    Remove debugging; the problem was that puppetmaster needs to be restarted when these files change

--- include/include_file.rb
+++ include/include_file.rb
@@ -1,15 +1,11 @@
 
 def include_file( name )
-	Puppet.notice( "name = " + name + ", templatedir = " + templatedir )
-
 	# add the path separator if it's not already there
 	templatedir = scope.lookupvar("templatedir")
 	templatedir.concat("/") unless templatedir.endswith("/")
 
 	includeheader = "#####\n# Included file: \n# " + templatedir + name + "\n#####\n"
 	includefooter = "#####\n# End of included file: \n# " + templatedir + name + "\n#####\n"
-
-	Puppet.notice( "name = " + name + ", templatedir = " + templatedir )
 
 	if File.exists?( templatedir + name ) then
 		return includeheader + IO.read( templatedir + name ) + includefooter
