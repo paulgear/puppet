5e25afb40e1f196b9e1e5a73cb06aaceb7dab57b
Paul Gear <github@libertysys.com.au>
2010-11-24 12:48:21 +1000

    With source, puppet copies symlinks as symlinks, even though the manifest specifies file

--- modules/network/manifests/interfaces.pp
+++ modules/network/manifests/interfaces.pp
@@ -11,7 +11,7 @@ class network::interfaces {
 		owner	=> root,
 		group	=> root,
 		mode	=> 644,
-		source	=> "puppet:///modules/network/interfaces/$fqdn",
+		content	=> generate("/bin/cat", "/etc/puppet/modules/network/interfaces/$fqdn"),
 	}
 
 }
