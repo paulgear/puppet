c34827cf9a95854ae358d2a0aae71e52839207f6
Paul Gear <github@libertysys.com.au>
2010-12-30 15:52:33 +1000

    Ensure amavis & clamav can talk to each other

--- modules/amavis/manifests/init.pp
+++ modules/amavis/manifests/init.pp
@@ -3,11 +3,28 @@
 #
 
 class amavis {
+	include amavis::package
+	include amavis::groups
+}
 
+class amavis::package {
 	$pkg = "amavisd-new"
-
 	package { $pkg:
 		ensure		=> installed
+	}
+}
+
+class amavis::groups {
+	$group = [ "amavis", "clamav" ]
+	user { "amavis":
+		groups		=> $groups,
+		membership	=> minimum,
+		require		=> Class["amavis::package"],
+	}
+	user { "clamav":
+		groups		=> $groups,
+		membership	=> minimum,
+		require		=> Class["amavis::package"],
 	}
 }
 
