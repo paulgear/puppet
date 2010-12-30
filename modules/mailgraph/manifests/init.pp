8b2b69587de008f0652002ec08abeca42633c827
Paul Gear <github@libertysys.com.au>
2010-12-30 14:59:20 +1000

    Revamp mailgraph and enable on Debian

--- modules/mailgraph/manifests/init.pp
+++ modules/mailgraph/manifests/init.pp
@@ -3,7 +3,11 @@
 #
 
 class mailgraph {
+	include mailgraph::package
+	include mailgraph::service
+}
 
+class mailgraph::package {
 	$pkg = "mailgraph"
 
 	package { $pkg:
@@ -11,4 +15,28 @@ class mailgraph {
 	}
 }
 
+class mailgraph::service {
+	$svc = "mailgraph"
+
+	service { $svc:
+		enable		=> true,
+		hasstatus	=> true,
+		hasrestart	=> true,
+		require		=> Class["mailgraph::package"],
+	}
+
+	# manually enable on Debian/Ubuntu
+	case $operatingsystem {
+		debian, ubuntu: {
+			text::replace_lines { "/etc/default/mailgraph":
+				file		=> "/etc/default/mailgraph",
+				pattern		=> "^BOOT_START=.*",
+				replace		=> "BOOT_START=true",
+				optimise	=> true,
+				notify		=> Service[$svc],
+				require		=> Class["mailgraph::package"],
+			}
+		}
+	}
+}
 
