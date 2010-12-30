6b32be2a57311052fa72606ce795c006cd3443f0
Paul Gear <github@libertysys.com.au>
2010-12-30 16:52:57 +1000

    Enable razor logging via syslog

--- modules/razor/manifests/init.pp
+++ modules/razor/manifests/init.pp
@@ -1,12 +1,26 @@
 # puppet class to install razor
 
 class razor {
+	include razor::package
+	include razor::logging
+}
 
+# set razor to log using syslog
+class razor::logging {
+	$file = "/etc/razor/razor-agent.conf"
+	text::replace_lines { $file:
+		file		=> $file,
+		pattern		=> '^logfile = .*',
+		replace		=> 'logfile = sys-syslog',
+		optimise	=> true,
+		require		=> Class["razor::package"],
+	}
+}
+
+class razor::package {
 	$pkg = "razor"
-
 	package { $pkg:
 		ensure	=> installed,
 	}
-
 }
 
