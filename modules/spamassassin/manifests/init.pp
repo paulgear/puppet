a29f11b136280c45757fdd3f0c27051d281d7e9d
Paul Gear <github@libertysys.com.au>
2010-12-30 12:42:30 +1000

    Revamp SpamAssassin class to allow per-host customisation of trusted_networks & whitelist_from
    Note that this means that calling spamassassin::config() is required to create local.cf.

--- modules/spamassassin/manifests/init.pp
+++ modules/spamassassin/manifests/init.pp
@@ -1,66 +1,90 @@
-#
-# Puppet example class for SpamAssassin
-#
-# DONE: Edited for Ubuntu compatibility
-#
+# puppet class to configure SpamAssassin
 
 class spamassassin {
+	include spamassassin::package
+	include spamassassin::service
+	include spamassassin::ruleupdate
+}
 
-	# determine package, service, and directory names
+# install package
+class spamassassin::package {
 	$pkg = "spamassassin"
+	package { $pkg:
+		ensure		=> installed,
+	}
+}
+
+# enable nightly rule updates
+class spamasssassin::ruleupdate {
+	case $operatingsystem {
+		centos: {
+			cron_job { "spamassassin-rule-update":
+				interval	=> daily,
+				script		=> '#!/bin/sh
+# Created by puppet - do not edit here
+/usr/share/spamassassin/sa-update.cron
+',
+				require		=> Class[spamassassin::package],	# I wonder if there's a way to set this once for the whole class...
+			}
+		}
+		debian, ubuntu: {
+			$file = "/etc/default/spamassassin"
+			text::replace_lines { $file:
+				file		=> $file,
+				pattern		=> "^CRON=.*",
+				replace		=> "CRON=1",
+				optimise	=> true,
+				require		=> Class[spamassassin::package],	# I wonder if there's a way to set this once for the whole class...
+			}
+		}
+	}
+}
+
+# enable service
+class spamassassin::service {
 	$svc = "spamassassin"
+	service { $svc:
+		enable		=> true,
+		hasrestart	=> true,
+		hasstatus	=> true,
+		require		=> Class[spamassassin::package],	# I wonder if there's a way to set this once for the whole class...
+	}
+	# on these operating systems, there is no standardised way to enable/disable a service :-(
+	case $operatingsystem {
+		debian, ubuntu: {
+			$file = "/etc/default/spamassassin"
+			text::replace_lines { $file:
+				file		=> $file,
+				pattern		=> "^ENABLED=.*",
+				replace		=> "ENABLED=1",
+				optimise	=> true,
+				notify		=> Service[$svc],
+				require		=> Class[spamassassin::package],	# I wonder if there's a way to set this once for the whole class...
+			}
+		}
+	}
+}
+
+# this must be called to install a customised version of the spamassassin configuration
+define spamassassin::config (
+		$trusted_networks = [],
+		$whitelist_from = []
+		) {
+	include spamassassin
+	$templatedir = "/etc/puppet/modules/spamassassin/templates"
 	$dir = $operatingsystem ? {
 		centos		=> "/etc/mail/spamassassin",
 		redhat		=> "/etc/mail/spamassassin",
 		default		=> "/etc/spamassassin",
 	}
-
-	package { $pkg:
-		ensure		=> installed,
-	}
-
-	service { $svc:
-		enable		=> true,
-		hasrestart	=> true,
-		hasstatus	=> true,
-		require		=> Package[$pkg],
-	}
-
-	$trusted_networks = [
-	]
-	$whitelist_from = [
-	]
-
-	$templatedir = "/etc/puppet/modules/spamassassin/templates"
 	file { "$dir/local.cf":
-		require		=> Package[$pkg],	# make sure the package is installed before creating this file
-		notify		=> Service[$svc],	# make sure the service is restarted after changing this file
+		require		=> Class[spamassassin::package],
+		notify		=> Class[spamassassin::service],
 		ensure		=> file,
 		owner		=> root,
 		group		=> root,
 		mode		=> 644,
 		content		=> template("spamassassin/local.cf.erb"),
 	}
-
-	# enable nightly spamassassin rule updates
-	case $operatingsystem {
-		centos: {
-			cron_job { "spamassassin-rule-update":
-				interval	=> daily,
-				script		=> '#!/bin/sh
-/usr/share/spamassassin/sa-update.cron
-',
-			}
-		}
-		debian, ubuntu: {
-			$default_file = "/etc/default/spamassassin"
-			exec { "spamassassin-rule-update":
-				command	=> "sed -i.BAK -e 's/\\(CRON\\|ENABLED\\)=0/\\1=1/' $default_file",
-				onlyif	=> "grep -e CRON=0 -e ENABLED=0 $default_file",
-				notify	=> Service[$svc],
-			}
-		}
-	}
-
 }
 
