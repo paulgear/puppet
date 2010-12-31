ce10881cc3f871255e875234eb260ff3cf6a1563
Paul Gear <github@libertysys.com.au>
2010-12-31 17:00:57 +1000

    Add maildrop config

--- modules/maildrop/manifests/init.pp
+++ modules/maildrop/manifests/init.pp
@@ -2,12 +2,25 @@
 
 class maildrop {
 	include maildrop::package
+	include maildrop::config
 }
 
 class maildrop::package {
 	$pkg = "maildrop"
 	package { $pkg:
 		ensure		=> installed,
+	}
+}
+
+# ensure maildrop delivers to maildirs
+class maildrop::config {
+	$cfg = "/etc/maildroprc"
+	text::replace_lines { $cfg:
+		file		=> $cfg,
+		pattern		=> '^#?DEFAULT=.*',
+		replace		=> 'DEFAULT="$HOME/Maildir"',
+		optimise	=> true,
+		require		=> Class["maildrop::package"],
 	}
 }
 
