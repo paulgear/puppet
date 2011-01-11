f567aae448d94f42edeea4418fa71397ddaef16e
Paul Gear <github@libertysys.com.au>
2011-01-11 16:20:52 +1000

    Revert "Don't use vmware tools on Debian & Ubuntu"
    This reverts commit d86c7880c6a5da8bcf7f833ca05ed045fb11ac92.

--- modules/vmware/manifests/tools.pp
+++ modules/vmware/manifests/tools.pp
@@ -6,34 +6,30 @@
 
 class vmware::tools {
 
-	if $operatingsystem == "centos" {
+	include	grub::default
 
-		include	grub::default
+	$pkgs = $operatingsystem ? {
+		centos	=> [ "gcc", "kernel-headers", "kernel-devel", ],
+		ubuntu	=> [ "build-essential", "linux-headers-server", "linux-source", ],
+		debian	=> [ "build-essential", "linux-headers-2.6", ],
+	}
 
-		$pkgs = $operatingsystem ? {
-			centos	=> [ "gcc", "kernel-headers", "kernel-devel", ],
-			ubuntu	=> [ "build-essential", "linux-headers-server", "linux-source", ],
-			debian	=> [ "build-essential", "linux-headers-2.6", ],
-		}
+	package { $pkgs:
+		ensure	=> installed,
+	}
 
-		package { $pkgs:
-			ensure	=> installed,
-		}
+	file { "/etc/init.d/vmware-tools-configure":
+		ensure	=> file,
+		owner	=> root,
+		group	=> root,
+		mode	=> 755,
+		source	=> "puppet:///modules/vmware/vmware-tools-configure.init.sh",
+		require	=> Package[ $pkgs ],
+	}
 
-		file { "/etc/init.d/vmware-tools-configure":
-			ensure	=> file,
-			owner	=> root,
-			group	=> root,
-			mode	=> 755,
-			source	=> "puppet:///modules/vmware/vmware-tools-configure.init.sh",
-			require	=> Package[ $pkgs ],
-		}
-
-		service { "vmware-tools-configure":
-			enable	=> true,
-			require	=> File[ "/etc/init.d/vmware-tools-configure" ],
-		}
-
+	service { "vmware-tools-configure":
+		enable	=> true,
+		require	=> File[ "/etc/init.d/vmware-tools-configure" ],
 	}
 
 }
