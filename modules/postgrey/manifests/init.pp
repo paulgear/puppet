959b420753cdbd7ce9ee7f5ede6119e863089b68
Paul Gear <github@libertysys.com.au>
2011-02-02 00:04:46 +1000

    Re-vamp postgrey class; add local files

--- modules/postgrey/manifests/init.pp
+++ modules/postgrey/manifests/init.pp
@@ -1,18 +1,38 @@
 # puppet class to configure postgrey
 
 class postgrey {
+	include postgrey::package
+	include postgrey::service
+	include postgrey::files
+}
 
+class postgrey::files {
+	define postgrey_file ( $replace = false ) {
+		file { "/etc/postgrey/$name":
+			ensure	=> file,
+			owner	=> root,
+			group	=> root,
+			mode	=> 644,
+			source	=> "puppet:///modules/postgrey/$name",
+			replace	=> $replace,
+		}
+	}
+	$files = [ "whitelist_clients.local", "whitelist_recipients.local" ]
+	postgrey_file { $files: }
+}
+
+class postgrey::package {
 	$pkg = "postgrey"
-	$svc = "postgrey"
-
 	package { $pkg:
 		ensure		=> installed,
 	}
+}
 
+class postgrey::service {
+	$svc = "postgrey"
 	service { $svc:
 		enable		=> true,
-		require		=> Package[$pkg],
+		require		=> Class["postgrey::package"],
 	}
-
 }
 
