1d481937be03f8f2ca7b043379f4f337335a67dd
Paul Gear <github@libertysys.com.au>
2011-02-09 22:13:29 +1000

    Add feature for creation of bind zone definitions (cherry picked from commit 64e30d38e4cf33b795b88620f463a349b433590b)

--- modules/bind/manifests/init.pp
+++ modules/bind/manifests/init.pp
@@ -1,40 +1,20 @@
-#
 # puppet class to manage bind name servers
-#
 
 class bind {
+	include bind::config
+	include bind::package
+	include bind::service
+}
 
-	$pkg = $operatingsystem ? {
-		debian		=> "bind9",
-		ubuntu		=> "bind9",
-		default		=> "bind",
-	}
-	$svc = $operatingsystem ? {
-		debian		=> "bind9",
-		ubuntu		=> "bind9",
-		default		=> "named",
-	}
-
-	package { $pkg:
-		ensure		=> installed,
-	}
-
-	service { $svc:
-		enable		=> true,
-		hasrestart	=> true,
-		hasstatus	=> true,
-		require		=> Package[$pkg],
+class bind::config {
+	$dir = $operatingsystem ? {
+		debian		=> "/etc/bind",
+		ubuntu		=> "/etc/bind",
+		centos		=> "/var/named/chroot",
 	}
 
 	if $operatingsystem == "CentOS" {
-		$mod = "bind"
-		$chroot = "/var/named/chroot"
-
-		package { "caching-nameserver":
-			ensure	=> installed,
-		}
-
-		file { "$chroot/etc":
+		file { "$dir/etc":
 			ensure	=> directory,
 			owner	=> root,
 			group	=> named,
@@ -49,21 +29,105 @@ class bind {
 			"named.slave.zones",
 		]
 		define named_etc_file () {
-			file { "$chroot/etc/$name":
+			file { "$dir/etc/$name":
 				ensure		=> file,
 				owner		=> root,
 				group		=> named,
 				mode		=> 640,
-				source		=> "puppet:///modules/$mod/$name",
+				source		=> "puppet:///modules/bind/$name",
 				require		=> Package[$pkg],
 				notify		=> Service[$svc],
 			}
 		}
 		named_etc_file { $etc_files: }
 	}
-	else {
-		# No extra setup on Debian & Ubuntu for now
-	}
+}
 
+class bind::package {
+	$pkg = $operatingsystem ? {
+		debian		=> "bind9",
+		ubuntu		=> "bind9",
+		centos		=> [ "bind", "caching-nameserver", ],
+	}
+	package { $pkg:
+		ensure		=> installed,
+	}
+}
+
+class bind::service {
+	$svc = $operatingsystem ? {
+		debian		=> "bind9",
+		ubuntu		=> "bind9",
+		centos		=> "named",
+	}
+	service { $svc:
+		enable		=> true,
+		hasrestart	=> true,
+		hasstatus	=> true,
+		require		=> Class["bind::package"],
+	}
+}
+
+class bind::setup {
+	# generate named.conf.local from fragments
+	include concat::setup
+	$zones = "${bind::config}::dir/named.conf.local"
+	concat { $zones:
+		owner	=> bind,
+		group	=> bind,
+		mode	=> 644,
+	}
+}
+
+# create concat fragment in the zone file
+define bind::zone (
+	$zone,
+	$type,
+	$file,
+	$forwarders,
+	$masters,
+	$order = 10
+) {
+	include bind
+	include bind::setup
+	concat::fragment { $zone:
+		target	=> "${bind::setup}::zones",
+		content	=> template("bind/zone-def.erb"),
+		order	=> $order,
+	}
+}
+
+define bind::master_zone ( $zone = undef ) {
+	if $zone == "" {
+		$zone = $name
+	}
+	bind::zone { $zone:
+		type		=> "master",
+		file		=> "master/$zone",
+		zone		=> $zone,
+	}
+}
+
+define bind::slave_zone ( $zone = undef, $masters ) {
+	if $zone == "" {
+		$zone = $name
+	}
+	bind::zone { $name:
+		type		=> "slave",
+		file		=> "slave/$zone",
+		masters		=> $masters,
+		zone		=> $zone,
+	}
+}
+
+define bind::forward_zone ( $zone = undef, $forwarders ) {
+	if $zone == "" {
+		$zone = $name
+	}
+	bind::zone { $name:
+		type		=> "forward",
+		zone		=> $zone,
+		forwarders	=> $forwarders,
+	}
 }
 
