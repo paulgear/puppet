5b27b4d2a1c13202dc7c2b6122c3cfb372f4ca9e
Paul Gear <github@libertysys.com.au>
2011-02-12 18:11:10 +1000

    Refactor and extend openvpn module

--- modules/openvpn/manifests/init.pp
+++ modules/openvpn/manifests/init.pp
@@ -1,12 +1,36 @@
 # puppet class to install openvpn
 
 class openvpn {
+	include	openvpn::package
+	include openvpn::service
+}
 
+class openvpn::package {
 	$pkg = "openvpn"
-
 	package { $pkg:
 		ensure	=> installed,
 	}
+}
 
+class openvpn::service {
+	$svc = "openvpn"
+	service { $svc:
+		enable	=> true,
+		require	=> Class["openvpn::package"],
+	}
+}
+
+class openvpn::config {
+	include openvpn
+	$dir = "/etc/openvpn"
+	file { $dir:
+		ensure	=> directory,
+		recurse	=> true,
+		owner	=> root,
+		group	=> root,
+		source	=> "puppet:///modules/openvpn/$fqdn",
+		require	=> Class["openvpn::package"],
+		notify	=> Class["openvpn::service"],
+	}
 }
 
