6a18330f6456f788c3b74a01ce205ce179bbf810
Paul Gear <github@libertysys.com.au>
2011-03-15 11:40:20 +1000

    Add interface changing definition

--- modules/arpwatch/manifests/init.pp
+++ modules/arpwatch/manifests/init.pp
@@ -22,3 +22,19 @@ class arpwatch::service {
 	}
 }
 
+define arpwatch::interface ( $interface = "eth0" ) {
+	$cfg = "/etc/default/arpwatch"
+	$check = "/bin/grep -Ee '^ARGS=.*-i $interface' $cfg"
+	exec { "$cfg change interface":
+		command		=> "sed -r -i -e '/^ARGS=/s/-i [0-9A-Za-z]+/-i $interface/' $cfg",
+		unless		=> $check,
+		logoutput	=> true,
+	}
+	exec { "$cfg add interface":
+		command		=> "sed -r -i -e 's/^(ARGS=\".*)\"$/\1 -i $interface\"/' $cfg",
+		unless		=> $check,
+		require		=> Exec["$cfg change interface"],
+		logoutput	=> true,
+	}
+}
+
