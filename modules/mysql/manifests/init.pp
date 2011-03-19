8ed6edf5f820289b99caf9dbb8bcaf17ed6623a2
Paul Gear <github@libertysys.com.au>
2011-03-19 16:26:57 +1000

    Add mysql dump script

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -3,6 +3,7 @@
 class mysql::server {
 	include mysql::server::package
 	include mysql::server::service
+	include mysql::server::scripts
 }
 
 class mysql::server::package {
@@ -19,6 +20,23 @@ class mysql::server::service {
 		hasrestart	=> true,
 		hasstatus	=> true,
 		require		=> Class["mysql::server::package"],
+	}
+}
+
+class mysql::server::scripts {
+	$script = "/usr/local/bin/mysql-dumpall"
+	file { $script:
+		ensure		=> file,
+		mode		=> 755,
+		owner		=> root,
+		group		=> root,
+		source		=> "puppet:///modules/mysql/mysql-dumpall",
+	}
+	cron_job { "mysql-dumpall":
+		interval	=> "daily",
+		script		=> "#!/bin/sh
+$script
+",
 	}
 }
 
