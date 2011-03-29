a670df66896bf063dbc05d3bcccb794061777aa1
Paul Gear <github@libertysys.com.au>
2011-03-29 12:29:33 +1000

    Add CentOS support for Observium

--- modules/observium/manifests/init.pp
+++ modules/observium/manifests/init.pp
@@ -5,32 +5,61 @@ class observium {
 }
 
 class observium::packages {
-	$deps = [
+	include apache
+	include php5
+	include snmp
+
+	$common_pkgs = [
 
 		"fping",
 		"graphviz",
 		"imagemagick",
+		"nmap",
+		"php-pear",
+		"rrdtool",
+		"subversion",
+
+	]
+	$deb_pkgs = [
+
 		"ipcalc",
 		"ipmitool",
 		"libapache2-mod-php5",
 		"mtr-tiny",
 		"mysql-client",
-		"nmap",
 		"php5-cli",
 		"php5-gd",
 		"php5-mysql",
 		"php5-snmp",
 		"php5-xcache",
-		"php-pear",
-		"rrdtool",
 		"sipcalc",
 		"snmp",
 		"snmp-mibs-downloader",
-		"subversion",
 		"whois",
 
 	]
-	package { $deps:
+	$centos_pkgs = [
+
+		"jwhois",
+		"mysql",
+		"net-snmp-utils",
+		"OpenIPMI-tools",
+		"php-gd",
+		"php-mysql",
+		"php-pecl-apc",
+		"php-snmp",
+
+	]
+
+	$ospkgs = $operatingsystem ? {
+		Debian	=> $deb_pkgs,
+		Ubuntu	=> $deb_pkgs,
+		CentOS	=> $centos_pkgs,
+	}
+	package { $common_pkgs:
+		ensure	=> installed,
+	}
+	package { $ospkgs:
 		ensure	=> installed,
 	}
 }
