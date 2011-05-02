9323a7b805185a0429bef2f5fbdb2f08a4a84d72
Paul Gear <github@libertysys.com.au>
2011-05-02 11:14:53 +1000

    Add hostname fix for Debian

---
+++ modules/hostname/manifests/init.pp
@@ -0,0 +1,16 @@
+# hostname utilities for puppet
+
+# fixes default hostname entry on Debian, which has no fqdn present
+class hostname::setfqdn {
+	host { $fqdn:
+		ensure	=> present,
+		alias	=> [ $hostname ],
+		ip	=> '127.0.1.1',
+	}
+	host { $hostname:
+		ensure	=> absent,
+		alias	=> [ $hostname ],
+		ip	=> '127.0.1.1',
+	}
+}
+
