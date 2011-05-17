4eae65d1063d9704b6f22de378ea0a34bd79ebce
Paul Gear <github@libertysys.com.au>
2011-05-17 14:44:54 +1000

    Testing versions of crypto fs create/mount scripts

---
+++ modules/backup/manifests/cryptsetup.pp
@@ -0,0 +1,16 @@
+# puppet class to manage encrypted filesystem setup
+
+class backup::cryptsetup {
+	include backup::cryptsetup::files
+}
+
+class backup::cryptsetup::files {
+	ulb { [ "cryptfs-create", "cryptfs-mount", ]:
+		source_class	=> "backup::cryptsetup::files",
+	}
+	file { "/usr/local/bin/cryptfs-umount":
+		ensure		=> link,
+		target		=> "/usr/local/bin/cryptfs-mount",
+	}
+}
+
