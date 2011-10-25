46c89d40d757fbf560e81d57a25da1cd9196275b
Paul Gear <github@libertysys.com.au>
2011-10-25 12:41:34 +1000

    Add notify

--- modules/inittab/manifests/init.pp
+++ modules/inittab/manifests/init.pp
@@ -1,4 +1,11 @@
-# puppet module to mangle inittab
+# puppet module to mangle inittab - tested only on CentOS 5
+
+class inittab::notify {
+	exec { "/sbin/telinit q":
+		logoutput	=> true,
+		refreshonly	=> true,
+	}
+}
 
 define inittab::setdefault( $level ) {
 	include text
@@ -7,6 +14,7 @@ define inittab::setdefault( $level ) {
 		pattern		=> "^id:[^:]*:initdefault:",
 		replace		=> "id:$level:initdefault:",
 		optimise	=> true,
+		notify		=> Class["inittab::notify"],
 	}
 	
 }
@@ -18,11 +26,13 @@ define inittab::ctrlaltdel( $enable ) {
 			file		=> "/etc/inittab",
 			pattern		=> "^#.*ca::ctrlaltdel:",
 			replace		=> "ca::ctrlaltdel:",
+			notify		=> Class["inittab::notify"],
 		}
 	} else {
 		text::comment_lines { "disable ctrlaltdel":
 			file		=> "/etc/inittab",
 			pattern		=> "^ca::ctrlaltdel:",
+			notify		=> Class["inittab::notify"],
 		}
 	}
 }
