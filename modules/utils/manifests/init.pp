49aff19d171b5c80c7fb82cff581cb326cdeb7da
Paul Gear <github@libertysys.com.au>
2011-11-17 17:54:30 +1000

    Revamp handling of epel & rpmforge

--- modules/utils/manifests/init.pp
+++ modules/utils/manifests/init.pp
@@ -1,34 +1,32 @@
 #
 # Some basic utilities
 #
+# This is an icky class - it should be removed
+#
 # DONE: Edited for Ubuntu compatibility
 #
 
 class utils {
 	if ($operatingsystem == "CentOS") {
 
-		# define rpmforge yum repo
-		yumrepo { "rpmforge":
-			baseurl		=> 'http://apt.sw.be/redhat/el5/en/$basearch/rpmforge',
-			descr		=> 'Red Hat Enterprise $releasever - RPMforge.net - dag',
+		yumrepo { "epel":
 			enabled		=> 1,
-			includepkgs	=> 'clam*,sarg*,memtester*',
-			gpgcheck	=> 1,
-			gpgkey		=> 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag',
-			mirrorlist	=> 'http://apt.sw.be/redhat/el5/en/mirrors-rpmforge',
-			protect		=> 0,
+			includepkgs	=> 'epel-release,fail2ban*,git*,lzo*,openvpn*,perl-Git*,php-pecl-apc,pkcs11-helper*,rsnapshot',
+			require		=> Package["epel-release"],
 		}
 
-		# define EPEL yum repo
-		yumrepo { "epel":
-			baseurl		=> absent,
-			descr		=> 'Extra Packages for Enterprise Linux 5 - $basearch',
+		package { "epel-release":
+			ensure		=> installed,
+		}
+
+		yumrepo { "rpmforge":
 			enabled		=> 1,
-			gpgcheck	=> 1,
-			gpgkey		=> 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL',
-			mirrorlist	=> 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-5&arch=$basearch',
-			protect		=> 0,
-			includepkgs	=> 'fail2ban*,git*,lzo*,openvpn*,perl-Git*,php-pecl-apc,pkcs11-helper*,rsnapshot',
+			includepkgs	=> 'rpmforge-release,clam*,graphviz,ifplugd,imapsync,lua*,memtester*,perl-*,rrdtool*,sarg*',
+			require		=> Package["rpmforge-release"],
+		}
+
+		package { "rpmforge-release":
+			ensure		=> installed,
 		}
 
 	}
