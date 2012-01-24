16f0711fe17c04017a9e541832326f4f095359dc
Paul Gear <github@libertysys.com.au>
2012-01-25 08:28:38 +1000

    Add rudimentary function to change crontab hour - needs more work.

---
+++ modules/cron/manifests/init.pp
@@ -0,0 +1,13 @@
+# puppet module to manage crontab file on linux
+
+# change the hour of the cron daily/weekly/monthly run
+# FIXME: this rewrites the crontab file on every run
+define cron::crontab ( $hour ) {
+	include text
+	$crontab = "/etc/crontab"
+	text::replace_lines { $crontab:
+		file		=> $crontab,
+		pattern		=> "^([0-9]+)[ 	]+[0-9]+[ 	]+",
+		replace		=> "\1 $hour ",
+	}
+}
