005bf8a06e2fc57a2949f81ebecb4076f016715b
Paul Gear <github@libertysys.com.au>
2012-02-28 14:13:08 +1000

    Add smartd/smartmontools

---
+++ modules/smart/manifests/init.pp
@@ -0,0 +1,48 @@
+# puppet class to manage smartd/smartmontools
+
+class smartd {
+	include smartd::package
+	include smartd::service
+}
+
+# rudimentary smartd.conf
+class smartd::config {
+	include smartd
+	exec { "backup smartd.conf":
+		command		=> "cp -a /etc/smartd.conf /etc/smartd.conf.pre-puppet",
+		creates		=> "/etc/smartd.conf.pre-puppet",
+		logoutput	=> on_failure,
+		require		=> Class["smartd::package"],
+	}
+	file { "/etc/smartd.conf":
+		notify		=> Class["smartd::service"],
+		require		=> Exec["backup smartd.conf"],
+		mode		=> 644,
+		owner		=> root,
+		group		=> root,
+		content		=> "# managed by puppet - do not edit here
+# See /etc/smartd.conf.pre-puppet for original
+DEVICESCAN -H -m root
+",
+	}
+}
+
+class smartd::package {
+	$pkg = "smartmontools"
+	package { $pkg:
+		ensure		=> installed,
+	}
+}
+
+class smartd::service {
+	$svc = $operatingsystem ? {
+		"centos"	=> "smartd",
+		"redhat"	=> "smartd",
+		default		=> "smartmontools",
+	}
+	service { $svc:
+		enable		=> true,
+		require		=> Class["smartd::package"],
+	}
+}
+
