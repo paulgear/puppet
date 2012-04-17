42b208302a355d21a9e277b9b29894a5136b6a2c
Paul Gear <github@libertysys.com.au>
2012-04-17 14:17:17 +1000

    Add class to run newaliases when alias is added

--- modules/mail/manifests/init.pp
+++ modules/mail/manifests/init.pp
@@ -1,10 +1,25 @@
-#
-# action to send mail
-#
+# puppet mail utilities
 
+# action to send mail
 define send_mail( $message ) {
 	exec { "mail $message":
 		command		=> "sh -c 'echo $message | mail -s puppet root@localhost'",
 		refreshonly	=> true,
 	}
 }
+
+# run newaliases whenever an alias is added
+class mail::newaliases {
+	include mail::newaliases::exec
+	Mailalias {
+		notify => Class["mail::newaliases::exec"],
+	}
+}
+
+class mail::newaliases::exec {
+	exec { "newaliases":
+		logoutput	=> on_failure,
+		refreshonly	=> true,
+	}
+}
+
