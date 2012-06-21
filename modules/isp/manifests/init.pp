84577cd34f27ac87448fe9647d9215da914a529f
Paul Gear <github@libertysys.com.au>
2012-06-21 15:18:58 +1000

    Add CentOS mirror base

--- modules/isp/manifests/init.pp
+++ modules/isp/manifests/init.pp
@@ -5,24 +5,28 @@ class isp {
 
 		"iinet", "netspace": {
 			$mirrorbase = "http://ftp.iinet.net.au"
+			$centosbase = "$mirrorbase/pub/centos"
 			$debianbase = "$mirrorbase/debian/debian"
 			$debianbackports = "$mirrorbase/pub/debian-backports"
 		}
 
 		"exetel": {
 			$mirrorbase = "http://mirror.exetel.com.au/"
+			$centosbase = "http://mirror.aarnet.edu.au/pub/centos"
 			$debianbase = "$mirrorbase/pub/debian/debian"
 			$debianbackports = "$mirrorbase/pub/debian/debian-backports"
 		}
 
 		"bigpond": {
 			$mirrorbase = "http://mirror.aarnet.edu.au/"
+			$centosbase = "$mirrorbase/pub/centos"
 			$debianbase = "$mirrorbase/pub/debian"
 			$debianbackports = "http://ftp.iinet.net.au/pub/debian-backports"
 		}
 
 		default: {
 			$mirrorbase = "http://ftp.au.debian.org"
+			$centosbase = "http://mirror.aarnet.edu.au/pub/centos"
 			$debianbase = "$mirrorbase/debian"
 			$debianbackports = "$mirrorbase/backports.org"
 		}
