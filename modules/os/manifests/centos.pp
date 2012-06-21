155dc1a619c45c19d4d1a4d3703d6d4c431f6122
Paul Gear <github@libertysys.com.au>
2012-06-21 15:19:31 +1000

    Add manual control of CentOS 6 repos

--- modules/os/manifests/centos.pp
+++ modules/os/manifests/centos.pp
@@ -20,9 +20,66 @@ define os::centos::initstyle ( $style = "color" ) {
 	include text
 	$file = "/etc/sysconfig/init"
 	text::replace_add_line { $file:
-		file	=> $file,
-		pattern => "^BOOTUP=",
-		line	=> "BOOTUP=$style",
+		file		=> $file,
+		pattern		=> "^BOOTUP=",
+		line		=> "BOOTUP=$style",
 	}
+}
+
+class os::centos::6::yumrepos {
+	include isp
+
+	$default_repo_file = "/etc/yum.repos.d/CentOS-Base"
+	$repobase = "$isp::mirrorbase/$operatingsystemrelease"
+
+	file { $default_repo_file:
+		ensure		=> absent,
+	}
+
+	yumrepo { "centos-base-puppet":
+		baseurl		=> "$repobase/os/$architecture/",
+		enabled		=> 1,
+		gpgcheck	=> 1,
+		gpgkey		=> "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6",
+		mirrorlist	=> absent,
+		require		=> File[$default_repo_file],
+	}
+
+	yumrepo { "centos-updates-puppet":
+		baseurl		=> "$repobase/updates/$architecture/",
+		enabled		=> 1,
+		gpgcheck	=> 1,
+		gpgkey		=> "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6",
+		mirrorlist	=> absent,
+		require		=> File[$default_repo_file],
+	}
+
+	yumrepo { "centos-extras-puppet":
+		baseurl		=> "$repobase/extras/$architecture/",
+		enabled		=> 1,
+		gpgcheck	=> 1,
+		gpgkey		=> "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6",
+		mirrorlist	=> absent,
+		require		=> File[$default_repo_file],
+	}
+
+	yumrepo { "centos-centosplus-puppet":
+		baseurl		=> "$repobase/centosplus/$architecture/",
+		enabled		=> 0,
+		gpgcheck	=> 1,
+		gpgkey		=> "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6",
+		mirrorlist	=> absent,
+		require		=> File[$default_repo_file],
+	}
+
+	yumrepo { "centos-contrib-puppet":
+		baseurl		=> "$repobase/contrib/$architecture/",
+		enabled		=> 0,
+		gpgcheck	=> 1,
+		gpgkey		=> "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6",
+		mirrorlist	=> absent,
+		require		=> File[$default_repo_file],
+	}
+
 }
 
