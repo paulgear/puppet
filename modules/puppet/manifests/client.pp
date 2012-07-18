a6bed7f0d1234eaf00b1ece1e4f66faeb61913f0
Paul Gear <github@libertysys.com.au>
2012-07-19 08:17:21 +1000

    Initial testing of this script disconcertingly smooth - adding more error checking.

--- modules/puppet/manifests/client.pp
+++ modules/puppet/manifests/client.pp
@@ -49,6 +49,8 @@ class puppet::client::bucket_clean {
 	cron_job { "puppet-clientbucket-clean":
 		interval	=> "weekly",
 		script		=> "#!/bin/sh
+set -e
+set -u
 cd /var/lib/puppet/clientbucket
 du -mxs .
 df -m .
