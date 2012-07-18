d9a36229b350cf481b532a418c933150b50e6f18
Paul Gear <github@libertysys.com.au>
2012-07-19 08:04:22 +1000

    Modularise; add regular cleaning of clientbucket

--- modules/puppet/manifests/client.pp
+++ modules/puppet/manifests/client.pp
@@ -1,25 +1,30 @@
 #
 # puppet client management
 #
-# FIXME: This may not be needed any more.
-#
-# DONE: No changes required for Ubuntu compatibility
-#
 
 class puppet::client {
+	include puppet::client::bucket_clean
+	#include puppet::client::report
+	#include puppet::client::service
+	include puppet::client::weekly
+}
 
-	# restart puppet daemon weekly
-	cron_job { "puppet-restart":
-		interval	=> "weekly",
-		script		=> "#!/bin/sh
-/etc/init.d/puppet restart >/dev/null 2>&1
-"
+class puppet::client::report {
+	include puppet::client::service
+	$cfg = "/etc/puppet/puppet.conf"
+	$tag = $puppetversion ? {
+		/0\.25.*/	=> "puppetd",
+		default		=> "agent",
 	}
-
+	exec { "append agent section":
+		command		=> "echo '[$tag]\nreport = true\n' >> $cfg",
+		unless		=> "grep -q '^\\[$tag\\]' $cfg",
+		notify		=> Class["puppet::client::service"],
+		logoutput	=> true,
+	}
 }
 
 class puppet::client::daily {
-
 	# restart puppet daemon daily
 	cron_job { "puppet-restart-daily":
 		interval	=> "daily",
@@ -27,6 +32,31 @@ class puppet::client::daily {
 /etc/init.d/puppet restart >/dev/null 2>&1
 "
 	}
+}
 
+class puppet::client::weekly {
+	# restart puppet daemon weekly
+	cron_job { "puppet-restart":
+		interval	=> "weekly",
+		script		=> "#!/bin/sh
+/etc/init.d/puppet restart >/dev/null 2>&1
+"
+	}
+}
+
+class puppet::client::bucket_clean {
+	# clean out clientbucket
+	cron_job { "puppet-clientbucket-clean":
+		interval	=> "weekly",
+		script		=> "#!/bin/sh
+cd /var/lib/puppet/clientbucket
+du -mx
+df -m .
+find . -type f -mtime +99 -print0 | xargs -0 rm -f
+find . -type d -print0 | xargs -0 rmdir 2>/dev/null
+du -mx
+df -m .
+"
+	}
 }
 
