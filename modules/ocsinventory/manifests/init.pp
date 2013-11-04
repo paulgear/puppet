8b6ffa30f516c2f2e0eaeebdb9d5ac1193d82f94
Paul Gear <github@libertysys.com.au>
2013-11-04 19:18:51 +1000

    Revert "Add fix for CentOS ocsinventory-agent"
    This reverts commit 46fa12a97994d5039c32136091c000943db319cf.
    
    The dependency is actually present - problem misdiagnosed.

--- modules/ocsinventory/manifests/init.pp
+++ modules/ocsinventory/manifests/init.pp
@@ -3,12 +3,6 @@
 #
 
 class ocsinventory::package {
-	# CentOS RPM for ocsinventory-agent doesn't include required dependency
-	if $operatingsystem == "centos" {
-		package { "perl-XML-Simple":
-			ensure => installed,
-		}
-	}
 	$pkg = "ocsinventory-agent"
 	package { $pkg:
 		ensure => installed,
