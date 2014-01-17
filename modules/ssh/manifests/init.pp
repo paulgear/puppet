3cc769cb4f86424e4584c6122f47388d7fa03aab
Paul Gear <github@libertysys.com.au>
2014-01-17 12:59:21 +1000

    Do not optimise test

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -112,7 +112,7 @@ define ssh::without_password () {
 		file		=> $file,
 		pattern		=> '^PermitRootLogin.*',
 		replace		=> 'PermitRootLogin without-password',
-		optimise	=> true,
+		optimise	=> false,
 		require		=> Class["ssh::package"],
 		notify		=> Class["ssh::service"],
 	}
