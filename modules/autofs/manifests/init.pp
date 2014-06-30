399fe6c312d4bc45af84d90b5ee38163458a7e5d
Paul Gear <github@libertysys.com.au>
2014-06-30 14:16:56 +1000

    Update autofs class structure

--- modules/autofs/manifests/init.pp
+++ modules/autofs/manifests/init.pp
@@ -1,47 +1,46 @@
 #
 # puppet class to manage autofs
 #
-# DONE: Checked for Ubuntu compatibility
-#
 
 class autofs {
+	include autofs::package
+	include autofs::service
+}
 
+class autofs::package {
 	$pkg = "autofs"
-	$svc = "autofs"
-
 	package { $pkg:
 		ensure		=> installed,
 	}
+}
 
+class autofs::service {
+	$svc = "autofs"
 	service { $svc:
 		enable		=> true,
 		hasrestart	=> true,
 		hasstatus	=> true,
-		require		=> Package[$pkg],
+		require		=> Class["autofs::package"],
 	}
+}
 
-	define autofs( $device = "/dev/sdc1", $fstype = "ext4" ) {
-
-		file { "/etc/auto.master":
-			ensure		=> file,
-			owner		=> root,
-			group		=> root,
-			mode		=> 644,
-			require		=> Package[$autofs::pkg],
-			notify		=> Service[$autofs::svc],
-			content		=> template("autofs/auto.master.erb"),
-		}
-
-		file { "/etc/auto.autofs":
-			ensure		=> file,
-			owner		=> root,
-			group		=> root,
-			mode		=> 644,
-			require		=> [ Package[$autofs::pkg], File["/etc/auto.master"] ],
-			notify		=> Service[$autofs::svc],
-			content		=> template("autofs/auto.autofs.erb"),
-		}
-
+define autofs::autofs( $device = "/dev/sdc1", $fstype = "ext4" ) {
+	file { "/etc/auto.master":
+		ensure		=> file,
+		owner		=> root,
+		group		=> root,
+		mode		=> 644,
+		require		=> Class["autofs::package"],
+		notify		=> Class["autofs::service"],
+		content		=> template("autofs/auto.master.erb"),
 	}
-
+	file { "/etc/auto.autofs":
+		ensure		=> file,
+		owner		=> root,
+		group		=> root,
+		mode		=> 644,
+		require		=> [ Class["autofs::package"], File["/etc/auto.master"] ],
+		notify		=> Class["autofs::service"],
+		content		=> template("autofs/auto.autofs.erb"),
+	}
 }
