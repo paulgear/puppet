c4a1130d91bd5e29ab927c3e726edcbe1a782bf5
Paul Gear <github@libertysys.com.au>
2016-01-15 07:40:43 +1000

    Don't monitor rotated logs

--- modules/fail2ban/manifests/jails/apache.pp
+++ modules/fail2ban/manifests/jails/apache.pp
@@ -6,24 +6,24 @@
 class fail2ban::jails::apache {
 	fail2ban::jail { [ "apache-auth", "apache-badbots", "apache-nohome", "apache-noscript", "apache-overflows" ]:
 		action		=> '%(action_mwl)s',
-		logpath		=> '/var/log/apache*/*error*',
+		logpath		=> '/var/log/apache*/*error*log',
 		port		=> 'http,https',
 	}
 	fail2ban::jail { "apache-notfound":
 		findtime	=> 300,
-		logpath		=> '/var/log/apache*/*access*',
+		logpath		=> '/var/log/apache*/*access*log',
 		maxretry	=> 5,
 		port		=> 'http,https',
 	}
 	fail2ban::jail { "apache-denied":
 		findtime	=> 120,
-		logpath		=> '/var/log/apache*/*error*',
+		logpath		=> '/var/log/apache*/*error*log',
 		maxretry	=> 3,
 		port		=> 'http,https',
 	}
 	fail2ban::jail { "apache-generic-user":
 		findtime	=> 60,
-		logpath		=> '/var/log/apache*/*access*',
+		logpath		=> '/var/log/apache*/*access*log',
 		maxretry	=> 5,
 		port		=> 'http,https',
 	}
